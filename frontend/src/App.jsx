064780ca4d8cbdbb17a65840cd23239cd302b154
haroonstar2 <132174386+haroonstar2@users.noreply.github.com.>
2025-11-04 11:48:37 -0800

    Add Recipes page with Spoonacular API integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { Routes, Route, useNavigate} from "react-router-dom";
 
 import Calendar from "./app/calendar/Calendar";
 import Goal from "./app/calendar/Goal"
+import Recipes from "./app/recipes/recipes";
 
 export default function App() {
+  const navigate = useNavigate();
   const [selectedDay, setSelectedDay] = useState(1);
 
   // Goal consumes this
@@ -32,27 +35,35 @@ export default function App() {
   }
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>MacroTok</h1>
+    <Routes>
+      <Route path="/" element={
+        <div style={{ padding: 20 }}>
+          <h1>MacroTok</h1>
+          <p onClick={() => navigate("/app/recipes")}>Recipes</p>
 
-      {/* Demo buttons just to show intent to your team */}
-      <div style={{ display: "flex", gap: 8, margin: "12px 0 20px" }}>
-        <button className="cal-btn" onClick={loadDeficitExample}>Example: Deficit</button>
-        <button className="cal-btn" onClick={loadSurplusExample}>Example: Surplus</button>
-        <button className="cal-btn" onClick={() => setMealData({ breakfast:{calories:0}, lunch:{calories:0}, dinner:{calories:0} })}>
-          Clear
-        </button>
-      </div>
+          {/* Demo buttons just to show intent to your team */}
+          <div style={{ display: "flex", gap: 8, margin: "12px 0 20px" }}>
+            <button className="cal-btn" onClick={loadDeficitExample}>Example: Deficit</button>
+            <button className="cal-btn" onClick={loadSurplusExample}>Example: Surplus</button>
+            <button className="cal-btn" onClick={() => setMealData({ breakfast:{calories:0}, lunch:{calories:0}, dinner:{calories:0} })}>
+              Clear
+            </button>
+          </div>
 
-      <div style={{ marginBottom: 40 }}>
-        <Calendar
-          activeDay={selectedDay}
-          onPickDay={setSelectedDay}
-          onDayMealsChange={setMealData}  // Calendar updates Goal when meals change
-        />
-      </div>
+          <div style={{ marginBottom: 40 }}>
+            <Calendar
+              activeDay={selectedDay}
+              onPickDay={setSelectedDay}
+              onDayMealsChange={setMealData}  // Calendar updates Goal when meals change
+              />
+          </div>
 
-      <Goal dailyGoal={1900} mealData={mealData} />
-    </div>
+          <Goal dailyGoal={1900} mealData={mealData} />
+        </div>
+        }/>
+
+    {/* Recipes page */}
+    <Route path="/app/recipes" element={<Recipes/>}/>
+  </Routes>
   );
 }
